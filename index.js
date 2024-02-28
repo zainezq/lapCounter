355d70c9505dc15585a6eba00613252418b1f6ba
zainezq <zaineulabideen@outlook.com>
2024-02-28 12:37:48 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+let count = 0
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let initialSaveContent = saveEl.textContent; // Store the initial content
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+
+}
+
+function reset(){
+    if (confirm("Are you sure you want to reset the count?")) {
+        // Reset the count
+        
+        countEl.textContent = 0
+        count = 0
+        saveEl.textContent = initialSaveContent
+    }
+}
+
